cc76387408f653177a91b8b42f59c01bcf3fd420
kabir5145 <kabir10112006@gmail.com>
2025-08-29 22:45:28 +0530

    some changes in question

--- gfg/question 4/array_question.java
+++ gfg/question 4/array_question.java
@@ -1,17 +1,16 @@
+
 // Given an array of positive integers arr[], return the second largest element from the array. If the second largest element doesn't exist then return -1.
 import java.util.Arrays;
 
 public class array_question {
-    public static int secondLargest(int[] arr) {
+    public static void main(String[] args) {
+        int arr[] = { 2, 3, 6, 6, 5 };
+
         Arrays.sort(arr);
         int n = arr.length;
-        int index = n-1;
-        while(arr[index]==arr[n-1]){
+        int index = n - 1;
+        while (arr[index] == arr[n - 1]) {
             index--;
-            if(index<0){
-                return -1;
-            }
         }
-        return arr[index];
     }
 }
