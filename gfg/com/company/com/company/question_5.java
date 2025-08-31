7669ab33dcad425fe0c24503972ff825b42d0260
kabir5145 <kabir10112006@gmail.com>
2025-08-31 18:04:43 +0530

    finding the element in an 2D array

--- gfg/com/company/com/company/question_5.java
+++ gfg/com/company/com/company/question_5.java
@@ -1,7 +1,47 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class question_5 {
     public static void main(String[] args) {
-        
+        //Given a 2D integer array mat[][] and a number x, find whether element x is present in the matrix or not.
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Insert the numbers of rows :");
+        int a = sc.nextInt();
+        System.out.println("Insert the numbers of columns: ");
+        int b = sc.nextInt();
+        int[][] number = new int[a][b];
+        //the number to find
+        System.out.println("We have to find the number is:");
+        int x = sc.nextInt();
+
+        System.out.println("Now insert the numbers in the matrix in the following rows and columns:");
+
+        //input
+        for(int i = 0;i<a;i++){
+            for(int k = 0;k<b;k++){
+                number[i][k]= sc.nextInt();
+            }
+        }
+        for(int i = 0;i<a;i++){
+            for(int k = 0;k<b;k++){
+                System.out.print(number[i][k]+" ");
+            }
+            System.out.println();
+        }
+        boolean found = false;
+        for(int i = 0;i<a;i++){
+            for(int k = 0;k<b;k++){
+                if(number[i][k]==x){
+                    found = true;
+                    break;
+                }
+            }
+        }
+        if(found){
+            System.out.println("We have found the number !");
+        }else{
+            System.out.println("Not found the number !");
+        }
     }
 }
