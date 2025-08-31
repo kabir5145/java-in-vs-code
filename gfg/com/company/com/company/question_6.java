384a8f09a8be8c452ee7a1802c4a00995c66c6e3
kabir5145 <kabir10112006@gmail.com>
2025-08-31 18:37:08 +0530

    Array question to print the elements space-seperated

---
+++ gfg/com/company/com/company/question_6.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class question_6 {
+    public static void main(String[] args) {
+        //Given an array arr[], print all its elements space-separated.
+        int arr [] = {12,13,14,15,16};
+        for(int i = 0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
