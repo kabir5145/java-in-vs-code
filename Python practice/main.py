6ac828c88cfbf95083111661696ffeb92a795ef2
kabir5145 <kabir10112006@gmail.com>
2025-10-20 18:48:12 +0530

    Question 4

--- Python practice/main.py
+++ Python practice/main.py
@@ -18,7 +18,12 @@
 # print (sys.version_info)
 
 # Question-3 Current Date and Time
-import datetime
-now = datetime.datetime.now()
-print ("Current date and time : ")
-print (now.strftime("%Y-%m-%d %H:%M:%S"))
+# import datetime
+# now = datetime.datetime.now()
+# print ("Current date and time : ")
+# print (now.strftime("%Y-%m-%d %H:%M:%S"))
+
+# Question-4 Circle Area Calculator
+a = float(input("Input the radius of the circle : "))
+area = 3.14 * a**2
+print ("The area of the circle with radius " + str(a) + " is: " + str(area))
