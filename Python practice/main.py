d8ce55a2a487b11cf39c5b23fa05de740680340a
kabir5145 <kabir10112006@gmail.com>
2025-10-20 18:34:46 +0530

    Question-2

--- Python practice/main.py
+++ Python practice/main.py
@@ -1,9 +1,18 @@
 # Question-1 Formatted Twinkle Poem
 
-a = """Twinkle, twinkle, little star,
-	How I wonder what you are! 
-		Up above the world so high,   		
-		Like a diamond in the sky. 
-Twinkle, twinkle, little star, 
-	How I wonder what you are"""
-print(a)
+# a = """Twinkle, twinkle, little star,
+# 	How I wonder what you are! 
+# 		Up above the world so high,   		
+# 		Like a diamond in the sky. 
+# Twinkle, twinkle, little star, 
+# 	How I wonder what you are"""
+# print(a)
+
+# Question-2 Python Version Checker
+import sys
+
+print("Python version")
+print (sys.version)
+
+print("Version info.")
+print (sys.version_info)
