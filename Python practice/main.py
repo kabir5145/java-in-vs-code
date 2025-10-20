dc33a61bb9e71bf3787dca27985d0540e3481090
kabir5145 <kabir10112006@gmail.com>
2025-10-20 18:31:42 +0530

    Question 1

---
+++ Python practice/main.py
@@ -0,0 +1,9 @@
+# Question-1 Formatted Twinkle Poem
+
+a = """Twinkle, twinkle, little star,
+	How I wonder what you are! 
+		Up above the world so high,   		
+		Like a diamond in the sky. 
+Twinkle, twinkle, little star, 
+	How I wonder what you are"""
+print(a)
